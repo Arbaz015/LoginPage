7ff3edd26ec9460092cf7ce4304f3c1e5a4ba0f0
Arbaz015 <khanarbazmlk15@gmail.com>
2024-06-10 13:38:18 +0530

    User Signup Interface Completed

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -5,6 +5,9 @@ import android.text.InputFilter;
 import android.text.Spanned;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     StringBuilder result=new StringBuilder();
-    String name="";
+    String name="",pass="",add="",mob="";
+//    long mob
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
 
+//    RadioButton radioButton;
+;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup );
 
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
 
@@ -52,26 +62,68 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                   boolean check= isEmpty(binding.editTextText);
 
-                    if (check)
-                    {
-                        binding.editTextTextName.setError("Name Required");
+            if (binding.editTextTextName.getText().toString().length()==0)
+            {
+                binding.editTextTextName.setError("Name Required");
+
+            }
+            else {
+                name=binding.editTextTextName.getText().toString();
+                result.append("Name is \n");
+
+            }
+            if (binding.editTextTextPass.getText().toString().length()==0)
+            {
+                binding.editTextTextPass.setError("Password  Requird");
+            }
+            else {
+                pass=binding.editTextTextName.getText().toString();
+                result.append("Pass is \n");
 
 
-                    }else {
-                        name=binding.editTextTextName.getText().toString();
+            }
 
-                    }
+            int radioId=binding.radioGroup.getCheckedRadioButtonId();
+                if (radioId>0)
+                {
+                     radioButton=findViewById(radioId);
+                    result.append("Gender is \n");
+
+                }
+                else {
+                    binding.radioButtonMale.setError("Required");
+                    binding.radioButtonFemale.setError("Required");
+
+
+                }if (binding.editTextText.getText().toString().length()==0)
+            {
+                binding.editTextText.setError("Password  Requird");
+
+            }
+                else {
+                    add=binding.editTextText.getText().toString();
+                result.append("Add is \n");
+
+            }
+                if (binding.editTextPhone.getText().toString().length()==0)
+                {
+                    binding.editTextPhone.setError("Password  Requird");
+
+                }
+                else
+                {
+                    mob= binding.editTextPhone.getText().toString().toString();
+                    result.append("Phone is \n");
+
+
+                }
 
         });
-            binding.finalResult.setText(name);
-        Log.d("Check",""+name);
-    }
 
-    private  boolean isEmpty(EditText editText)
-    {
-       return binding.editTextTextName.getText().toString().trim().isEmpty();
+        binding.finalResult.setText(result);
 
     }
+
+
 }
